3db350a1dfb0de30e4c62bb70ddabbc62da98817
Jacob Johnson <jakeymacaroni@gmail.com>
2024-08-19 22:28:47 -0600

    updated static root settings for deployment

--- Plataforma_de_Vendas/Plataforma_de_Vendas/settings.py
+++ Plataforma_de_Vendas/Plataforma_de_Vendas/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path('/home/ec2-user/project_files/Plataforma_de_Vendas/static')
+BASE_DIR = Path('/home/ec2-user/project_files/Plataforma_de_Vendas')
 
 
 # Quick-start development settings - unsuitable for production
