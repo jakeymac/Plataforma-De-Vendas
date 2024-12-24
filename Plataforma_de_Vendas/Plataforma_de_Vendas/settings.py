2d1aead9e49f8fd78373452da7bb93e9e9787ee4
Jacob Johnson <jakeymacaroni@gmail.com>
2024-12-24 11:53:15 -0700

    aws setting update to use correct region

--- Plataforma_de_Vendas/Plataforma_de_Vendas/settings.py
+++ Plataforma_de_Vendas/Plataforma_de_Vendas/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Plataforma_de_Vendas', 'static')]
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = "plataforma-de-vendas"
-AWS_S3_REGION_NAME = "us-east-1"
+AWS_S3_REGION_NAME = "us-east-2"
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
